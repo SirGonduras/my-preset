6050092a65ba488e6eac08e2f51a2170299ba79e
SirGonduras <54453540+SirGonduras@users.noreply.github.com>
2020-09-28 02:54:11 +0200

    file include

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,58 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb()
+let projectFolder = "dist";
+let sourceFolder = "src";
+
+let path = {
+  build: {
+    html: projectFolder + "/",
+    css: projectFolder + "/css/",
+    js: projectFolder + "/js/",
+    img: projectFolder + "/img/",
+    fonts: projectFolder + "/fonts/"
+  },
+
+  src: {
+    html: sourceFolder + "/*.html",
+    less: sourceFolder + "/less/style.less",
+    js: sourceFolder + "/js/script.js",
+    img: sourceFolder + "/img/**/*.{jpg, png, svg, gif, ico, webm}",
+    fonts: sourceFolder + "/fonts/*.ttf"
+  },
+
+  watch: {
+    html: sourceFolder + "/**/*.html",
+    less: sourceFolder + "/less/**/*.less",
+    js: sourceFolder + "/js/**/*.js",
+    img: sourceFolder + "/img/**/*.{jpg, png, svg, gif, ico, webm}"
+  },
+  clean: "./" + projectFolder + "/"
+};
+
+let {src, dest} = require("gulp"),
+  gulp = require("gulp"),
+  browsersync = require("browser-sync").create(),
+  fileinclude = require("gulp-include");
+
+function browserSync(params) {
+  browsersync.init({
+    server: {
+      baseDir: "./" + projectFolder + "/"
+    },
+    port: 3000,
+    notify: false
+  });
 }
 
-exports.default = defaultTask
+function html(params) {
+  return src(path.src.html)
+    .pipe(fileinclude())
+    .pipe(dest(path.build.html))
+    .pipe(browsersync.stream());
+}
+
+let build = gulp.series(html);
+let watch = gulp.parallel(build, browserSync);
+
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
